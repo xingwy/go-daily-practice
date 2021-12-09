b15ba863a93642ca477e4f6bfa1115087fff9b98
xingwy <yuxingwei.code@foxmail.com>
2021-12-09 16:28:01 +0800

    Submit the solution of problem 剑指 Offer 31

--- Dec 2021/剑指 Offer 41.go
+++ Dec 2021/剑指 Offer 41.go
@@ -1,9 +1,17 @@
 
+// 数据流的中位数 两个堆的使用
 // 问题：如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
 // 设计一个支持以下两种操作的数据结构：
 // 1.void addNum(int num) - 从数据流中添加一个整数到数据结构中。
 // 2.double findMedian() - 返回目前所有元素的中位数。
 // 限制：最多会对addNum、findMedian进行50000次调用。
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
 
 type Heap struct {
 	data  []int
